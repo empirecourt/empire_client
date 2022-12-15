e784f53163b376a960af8bfae9a6f7d1e3b58fd1
Michael Simire <85413042+simiremichael@users.noreply.github.com>
2022-12-15 05:20:04 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useAppDispatch } from './app/store';
 import Booking from './components/Booking';
+import UserPrivateRoute from './components/UserPrivateRoute';
 import { setUsers } from './features/userSlice';
 import AllEvents from './pages/AllEvents';
 import Event from './pages/Event';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
+import Users from './pages/users';
 
 function App() {
 
@@ -19,10 +21,14 @@ useEffect(() => {
     <div>
      <Routes>
       <Route path='/' element={<Login />} />
-      {/* <Route path='/calendar' element={<Calender />} /> */}
+      <Route path='/users' element={<Users />} /> 
       <Route path='/adminPage' element={<AllEvents />} />
       <Route path='/event' element={<Event />} />
-      <Route path='/homepage' element={<HomePage />} />
+      <Route path='/homepage' element={
+      <UserPrivateRoute>
+      <HomePage />
+      </UserPrivateRoute>
+      } />
       <Route path='/booking' element={<Booking />} />
       </Routes>
     </div>
